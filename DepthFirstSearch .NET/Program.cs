32bac2fe1e5543163253d95031a92d6bfd3d8d8b
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2020-10-23 16:10:43 +0200

    Applied the suggested actions for improving code quality

--- DepthFirstSearch .NET/Program.cs
+++ DepthFirstSearch .NET/Program.cs
@@ -2,35 +2,36 @@
 
 namespace DepthFirstSearch_.NET
 {
-    class Program
+    internal class Program
     {
-        int[,] cost = {
+        private readonly int[,] _cost = {
             { 4, 7, 8, 6, 4 },
             { 6, 7, 3, 9, 2 },
             { 3, 8, 1, 2, 4 },
             { 7, 1, 7, 3, 7 },
             { 2, 9, 8, 9, 3 }
         };
-        static void Main(string[] args)
+
+        private static void Main(string[] args)
         {
             Console.WriteLine(new Program().MinCost(4, 4));
         }
-        
-        int MinCost(int m, int n) {
+
+        private int MinCost(int m, int n) {
             // wegkosten zur zelle (0,0) = kosten der zelle (0,0)
             if(m == 0 && n == 0) {
-                return cost[m, n];
+                return _cost[m, n];
             }
 
             // boundaries beachten!
             if(m == 0) {
-                return cost[m, n] + MinCost(m, n - 1);
+                return _cost[m, n] + MinCost(m, n - 1);
             }
             if(n == 0) {
-                return cost[m, n] + MinCost(m - 1, n);
+                return _cost[m, n] + MinCost(m - 1, n);
             }
             // rekursion
-            return cost[m, n] + Math.Min(MinCost(m - 1, n), MinCost(m, n - 1));
+            return _cost[m, n] + Math.Min(MinCost(m - 1, n), MinCost(m, n - 1));
         }
     }
 }
